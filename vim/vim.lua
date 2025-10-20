3ab2c85aab342c4ebd4cc954d55fb073105ac0a8
soulsploits <soeljei56@gmail.com>
2025-10-19 21:29:52 -0700

    Create vim.lua

---
+++ vim/vim.lua
@@ -0,0 +1,16 @@
+-- thanks to this guy: https://wearedevs.net/forum/t/27970
+local vim = game:GetService('VirtualInputManager')
+input = {
+    hold = function(key, time)
+        vim:SendKeyEvent(true, key, false, nil)
+        task.wait(time)
+        vim:SendKeyEvent(false, key, false, nil)
+    end,
+    press = function(key)
+        vim:SendKeyEvent(true, key, false, nil)
+	task.wait(0.005)
+        vim:SendKeyEvent(false, key, false, nil)
+    end
+}
+
+return input
